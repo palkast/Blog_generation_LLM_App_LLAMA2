60d5a89291c580ef2e1585938380df58f0b802f7
palkast <142919764+palkast@users.noreply.github.com>
2024-01-19 22:49:49 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+## Function To get response from LLAma 2 model
+
+def getLLamaresponse(input_text,no_words,blog_style):
+
+    ### LLama2 model
+    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+                      model_type='llama',
+                      config={'max_new_tokens':256,
+                              'temperature':0.01})
+    
+    ## Prompt Template
+
+    template="""
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+            """
+    
+    prompt=PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
+                          template=template)
+    
+    ## Generate the ressponse from the LLama 2 model
+    response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
+    print(response)
+    return response
+
+
+
+
+
+
+st.set_page_config(page_title="Generate Blogs",
+                    page_icon='🤖',
+                    layout='centered',
+                    initial_sidebar_state='collapsed')
+
+st.header("Generate Blogs 🤖")
+#input as text area 
+input_text=st.text_input("Enter the Blog Topic")
+
+## creating two more columns for additonal 2 fields one field how many words u want for that blog 
+## width of text box is 5,5 
+## col 2 blog style : for whom i am creating this blog , blog style will be drop down 
+
+col1,col2=st.columns([5,5])
+
+with col1:
+    no_words=st.text_input('No of Words')
+with col2:
+    blog_style=st.selectbox('Writing the blog for',
+                            ('Researchers','Data Scientist','Common People'),index=0)
+    
+submit=st.button("Generate")
+
+## Final response - get response from llama2 model using  input parameters : input text , no_words,blog_style 
+if submit:
+    st.write(getLLamaresponse(input_text,no_words,blog_style))
